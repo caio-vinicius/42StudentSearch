5dd96ae621c6b2080010f2ee02a641b728c8db2e
caio-vinicius <sf.caio92@gmail.com>
2020-07-14 18:33:59 -0300

    display abstracted information on tables

--- cli.py
+++ cli.py
@@ -6,9 +6,11 @@ import sys
 import time
 import pickle
 from terminaltables import AsciiTable, SingleTable
-from datetime import datetime
+from datetime import datetime, date
 import argparse
 import calendar
+from collections import Counter
+import os
 
 #validation args
 parser = argparse.ArgumentParser(description='Search for 42 student information.')
@@ -18,39 +20,30 @@ parser.add_argument('--no-cache',  dest='nocache', help='dont use cache')
 parser.add_argument('--photo', dest='photo', help='show intra url of current photo')
 args = parser.parse_args()
 
+#setup some api information
 intra_id = args.id;
 base_url = 'https://api.intra.42.fr'
-authorization = 'Bearer 21bacae2412f985f6779e714190f32c53f9525a6391c6a1b90028436a737abb6'
+authorization = 'Bearer XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 headers = {'Authorization': authorization}
 
+#id exists?
 try:
-	s = requests.get(f"{base_url}/v2/users/{intra_id}", headers=headers)
+	s = requests.get(f"{base_url}/v2/users/{intra_id}/titles", headers=headers)
 	s.raise_for_status()
 except requests.exceptions.HTTPError:
-	print('Sorry, unavailable id')
+	print('Sorry, something went wrong')
 	quit()
 
 sample_url = '{}/v2/users/{}'.format(base_url, intra_id)
 
 endpoints = [
-	'', 'apps', 'events', 'events_users',
-	'exams', 'coalitions', 'coalitions_users',
-	'cursus_users', 'campus_users', 'expertises_users',
-	'groups', 'groups_users', 'languages_users',
-	'locations', 'projects_users', 'quests_users',
-	'roles', 'scale_teams', 'scale_teams/as_corrector',
-	'scale_teams/as_corrected', 'tags', 'teams',
-	'titles', 'titles_users',
+	'', 'locations', 'apps', 'exams'
 ]
-
-endpoints = ['']
 
 formatedEndpoints = []
 
 for endpoint in endpoints:
 	formatedEndpoints.append(('{}/' + endpoint).format(sample_url))
-
-id_info = []
 
 #import os, fnmatch
 
@@ -64,6 +57,10 @@ id_info = []
 
 
 #if getCacheFile()[9:] == intra_id:
+
+
+#requistions happening here
+id_info = []
 
 try:
 	with open(f'.student-{intra_id}', 'rb') as f:
@@ -82,35 +79,82 @@ except EnvironmentError:
 			id_info.append(r)
 		else:
 			id_info.append({})
-		#time.sleep(0.5)
+		time.sleep(0.5)
+	date_requisition = datetime.now()
 	with open(f".student-{intra_id}", 'wb') as f:
 		pickle.dump(id_info, f)
 
-oncampus = 'No' if id_info[0]["location"] is None else f'Yes ({id_info[0]["location"]})'
-poolmonth = list(calendar.month_name).index(f'{id_info[0]["pool_month"]}'.title())
-bocal = 'No' if id_info[0]["staff?"] is False else 'Yes'
+#abstract some information
 
+#title
+title = ''
+if id_info[0]["titles"]:
+	title = f'{id_info[0]["titles"][0]["name"]}'
+else:
+	title = 'None'
+
+#on campus
+oncampus = 'No'
+if id_info[0]["location"]:
+	oncampus = f'Yes ({id_info[0]["location"]}'
+
+poolmonth = None
+if id_info[0]["pool_month"]:
+	poolmonth = list(calendar.month_name).index(f'{id_info[0]["pool_month"]}'.title())
+
+staff = 'No'
+if id_info[0]["staff?"]:
+	staff = 'Yes'
+
+#finished and in progress projects
 finished = 0
 inprogress = []
-for i, project in enumerate(id_info[0]["projects_users"]):
-	if project["status"] == 'finished':
-		finished += 1;
-	elif project["status"] == 'in_progress':
-		inprogress.append(project["project"]["name"])
+if id_info[0]["projects_users"]:
+	for i, project in enumerate(id_info[0]["projects_users"]):
+		if project["status"] == 'finished':
+			finished += 1;
+		elif project["status"] == 'in_progress' or 'waiting_for_correction':
+			inprogress.append(project["project"]["name"])
 
+
+#most apper of a pc
+if id_info[1]:
+	occurences = []
+	for location in id_info[1]:
+		occurences.append(location["host"][:6])
+	mostlocation = (Counter(occurences)).most_common(1)[0][0]
+
+#apps
+apps = []
+if id_info[2]:
+	for app in id_info[2]:
+		apps.append(app["name"])
+
+#place information
+#try:
 table_data = [
-		['Name:', f'{id_info[0]["first_name"]}'.title()],
-		['Email:', f'{id_info[0]["email"]}'.lower()],
+		['Name', id_info[0]["first_name"].title()],
+		['Title', title],
+		['Email:', id_info[0]["email"].lower()],
+		['Country', id_info[0]["campus"][0]["country"]],
 		['Campus', f'42 {id_info[0]["campus"][0]["name"]}'],
+		['Language', id_info[0]["campus"][0]["language"]["name"]],
 		['On Campus', oncampus],
-		['Pool', f'{poolmonth}/{id_info[0]["pool_year"]}'],
+		['Pool', None if not poolmonth else f'{poolmonth}/{id_info[0]["pool_year"]}'],
 		['Wallet', f'₳ {id_info[0]["wallet"]}'],
-		['Correction Points', f'{id_info[0]["correction_point"]}'],
-		['Is Bocal?', f'{bocal}'],
-		['Last Cursus', f'{id_info[0]["cursus_users"][len(id_info[0]["cursus_users"]) - 1]["cursus"]["name"]}'],
-		['Finished projects', f'{finished}'],
-		['In progress projects', f'{",".join(inprogress)}'],
-]
+		['Correction Points', id_info[0]["correction_point"]],
+		['Is Staff?', staff],
+		['Last Cursus', id_info[0]["cursus_users"][len(id_info[0]["cursus_users"]) - 1]["cursus"]["name"]],
+		['Finished projects', finished],
+		['In progress projects', None if not inprogress else ", ".join(inprogress)],
+		['Amount Achievements', len(id_info[0]["achievements"])],
+#		['More times on', mostlocation],
+		['Registred apps', None if not apps else ", ".join(apps)],
+		['Last exam', None if not id_info[3] else id_info[3][0]["name"]],
+		['Information obtained on', datetime.fromtimestamp(int(os.path.getctime(f".student-{intra_id}")))]
+	]
+#except IndexError as e:
+	#print("You need to make a new requistion: ", e)
 
 table = SingleTable(table_data, intra_id)
 table.inner_heading_row_border = False
